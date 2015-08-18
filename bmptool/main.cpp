1639ee20a82f4ad53b2b8edfdd9b97fbffc50e27
huziqin <huziqin@huziqindeMacBook-Pro.local>
2015-08-18 18:35:59 +0800

    Initial Commit

---
+++ bmptool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bmptool
+//
+//  Created by huziqin on 15/8/18.
+//  Copyright (c) 2015年 huziqin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
