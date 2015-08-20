e1868335dea4fed9cff158eb31a81b87ceff7972
huziqin <huziqin@huziqindeMacBook-Pro.local>
2015-08-20 23:54:37 +0800

    delete some comment

--- bmptool/main.cpp
+++ bmptool/main.cpp
@@ -5,15 +5,6 @@
 //  Created by huziqin on 15/8/18.
 //  Copyright (c) 2015年 huziqin. All rights reserved.
 //
-/*
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
-*/
 
 #include<stdio.h>
 #include "string.h"
